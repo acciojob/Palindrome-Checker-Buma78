5817de0b3350579e5d7dd4271746877aaaeea8e8
BHARAT PATIDAR <106677801+Buma78@users.noreply.github.com>
2023-02-02 13:20:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+     let str1 = str.tolowerCase();
+	let len = str1.length-1;
+	let start = 0;
+	while (start<len) {
+		if(str1.charAt(start)!=str1.charAt(len)){
+			return false;
+		}
+		start++;
+		len--;
+	}
+	return true;
+	
 }
 module.exports = palindrome
